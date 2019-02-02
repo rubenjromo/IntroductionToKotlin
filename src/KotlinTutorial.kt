114775c43635131b4a595905620e27ccb46aace5
RubenJromo <rjromo@espol.edu.ec>
2019-02-02 00:11:24 -0500

    Added sum +

--- src/KotlinTutorial.kt
+++ src/KotlinTutorial.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>) {
     val x = 1
     val y = 2
     println ("Total Sum: ${x+y}")
-    println ("Value of x: ${x} Value of y: ${y}")
+    println ("Value of x: ${x}\nValue of y: ${y}")
 
 
 }
